f8ac1ef8291d2a86418e8f4c0e237740799677eb
quochuy <quochuy20022001a@gmail.com>
2023-07-10 21:48:43 +0700

    first commit

---
+++ components/header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+
+    return (
+      <div className="Header">
+        
+      </div>
+    );
+  }
+}
+
+
+export default Header;
+
